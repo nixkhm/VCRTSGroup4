b822f13a85b81fa5b8f9c40bc8868e2ab017c0fd
Nicholas Masters <nicholas.masters20@stjohns.edu>
2022-09-28 17:13:43 -0400

    Owner Creation

--- OwnerCreation.java
+++ OwnerCreation.java
@@ -1,32 +1,31 @@
-import java.util.*;
+public class OwnerCreation {
 
-public class OwnerCreation{
     private String ownerName;
     private String ownerMake;
     private String ownerModel;
     private Double ownerYear;
 
-    public ownerCreation(private String ownerName, private String ownerMake, private String ownerModel, private double ownerYear,){
+    public OwnerCreation(String ownerName, String ownerMake, String ownerModel, double ownerYear) {
         this.ownerName = ownerName;
         this.ownerMake = ownerMake;
         this.ownerModel = ownerModel;
         this.ownerYear = ownerYear;
     }
 
-    public String getOwnerName(){
+    public String getOwnerName() {
         return ownerName;
     }
 
-    public String getOwnerMake(){
+    public String getOwnerMake() {
         return ownerMake;
     }
 
-    public String getOwnerModel(){
+    public String getOwnerModel() {
         return ownerModel;
     }
 
-    public double getOwnerYear(){
+    public double getOwnerYear() {
         return ownerYear;
     }
-    
+
 }
