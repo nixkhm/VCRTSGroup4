b108aaf2ba55397b0d6b8a894b044908d29d3c99
Sebastian <SebC89999@gmail.com>
2022-10-03 00:12:42 -0400

    Update registration.java

--- registration.java
+++ registration.java
@@ -19,35 +19,70 @@ public class registration {
         registrationTitle.setFont(new Font("Monospaced", Font.BOLD, 35));
         titlePanel.add(registrationTitle);
         dashboard.add(titlePanel);
-        //Panel that is supposed to include all the components. I will come back to this later.
-        
-        JPanel form = new JPanel();
-        form.setBackground(Color.LIGHT_GRAY);
-        form.setBounds(50,300,100,50);
-        
-        
+
+        //Panels that are supposed to include all the labels.
+        JPanel submitPanel = new JPanel();
+        submitPanel.setBounds(400, 500, 100,50);
+        JPanel carModelPanel = new JPanel();
+        carModelPanel.setBackground(Color.LIGHT_GRAY);
+        carModelPanel.setBounds(50,300,200,50);
+        JPanel carMakePanel = new JPanel();
+        carMakePanel.setBackground(Color.LIGHT_GRAY);
+        carMakePanel.setBounds(50,350,200,50);
+        JPanel carYearPanel = new JPanel();
+        carYearPanel.setBackground(Color.LIGHT_GRAY);
+        carYearPanel.setBounds(50,400,200,50);
+        JPanel ownerIdPanel = new JPanel();
+        ownerIdPanel.setBackground(Color.LIGHT_GRAY);
+        ownerIdPanel.setBounds(50,200,200,50);
+        JPanel durationOfRegistryPanel = new JPanel();
+        durationOfRegistryPanel.setBackground(Color.LIGHT_GRAY);
+        durationOfRegistryPanel.setBounds(50,250,200,50);
+
         //Labels for each text box.
-        JLabel OwnerID = new JLabel("Owner ID");
+        JLabel ownerID = new JLabel("Owner ID");
+        JLabel durationOfRegistry = new JLabel("Duration of registry input");
         JLabel carModel = new JLabel("Car Model");    
         JLabel carMake = new JLabel("Car Make");
         JLabel carYear = new JLabel("Car Year");
+        carModel.setBounds(50,300,50,50);  
+        ownerID.setBounds(50,200,50,50); 
+        durationOfRegistry.setBounds(50,250,50,50); 
+        carMake.setBounds(50,350,50,50); 
+        carYear.setBounds(50,400,50,50);     
+        carModelPanel.add(carModel);
+        carMakePanel.add(carMake);
+        carYearPanel.add(carYear);
+        ownerIdPanel.add(ownerID);
+        durationOfRegistryPanel.add(durationOfRegistry);
+
         //Setting up the text fields.
-        JTextField carModelInput, carMakeInput, carYearInput; 
-        carModel.setBounds(50,300,50,50);
-        form.add(carModel);
+        JTextField carModelInput, carMakeInput, carYearInput, ownerIdInput, durationOfRegistryInput;       
+        ownerIdInput = new JTextField();
+        ownerIdInput.setBounds(300,200,200,50);
+        durationOfRegistryInput = new JTextField();
+        durationOfRegistryInput.setBounds(300,250,200,50);
         carModelInput = new JTextField();
-        carModelInput.setBounds(250,300,200,50);
+        carModelInput.setBounds(300,300,200,50);
         carMakeInput = new JTextField();
-        carMakeInput.setBounds(250,400,200,50);
+        carMakeInput.setBounds(300,350,200,50);
         carYearInput = new JTextField();
-        carYearInput.setBounds(250,500,200,50);           
+        carYearInput.setBounds(300,400,200,50);     
+        
+        //Submit button
+        JButton submitButton = new JButton("Submit");
+        submitPanel.add(submitButton);
+        dashboard.add(submitPanel);
         dashboard.add(carModelInput);
         dashboard.add(carMakeInput);
         dashboard.add(carYearInput);
-        dashboard.add(carModel);
-        dashboard.add(carMake);
-        dashboard.add(carYear);
-        dashboard.add(form);
+        dashboard.add(ownerIdInput);
+        dashboard.add(durationOfRegistryInput);
+        dashboard.add(carModelPanel);
+        dashboard.add(carMakePanel);
+        dashboard.add(carYearPanel);
+        dashboard.add(ownerIdPanel);
+        dashboard.add(durationOfRegistryPanel);
         dashboard.setVisible(true);
 
     }
