5631d3a63d5a541084c42f8063319d30367f37fc
Sebastian <SebC89999@gmail.com>
2022-10-26 00:14:49 -0400

    git commit

--- BackEnd/Entities/CloudController.java
+++ BackEnd/Entities/CloudController.java
@@ -1,5 +1,25 @@
 package BackEnd.Entities;
+import java.util.*;
+import BackEnd.*;
+import BackEnd.Application.*;
 
 public class CloudController {
+      Queue<VehicleApplication> pendingVehicleApps;
+      Queue<JobApplication> pendingJobApps;
+      VehicularCloud currentVehicularCloud;
+      public void approveVehicle(VehicleApplication application, Vehicle vehicle){
 
+      }
+      public void denyVehicle(VehicleApplication application, Vehicle vehicle){
+
+      }
+      public void assignJobToVehicle(JobApplication jApplication, Job job, Vehicle vehicle){
+
+      }
+      public void denyJob(JobApplication job){
+
+      }
+      public VehicularCloud getVehicularCloud(){
+             return currentVehicularCloud;
+      }
 }
